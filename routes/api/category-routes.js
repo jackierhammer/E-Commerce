b5c1ce25dc6d8be1f0c2d37748f1bae8864331fb
Jackie <jackierhammer@gmail.com>
2022-09-22 12:58:25 -0600

    updated product-routes and minor reformatting

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,11 +14,11 @@ router.get('/', (req, res) => {
       }
     }
   )
-  .then(categoryData => res.json(categoryData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -33,11 +33,11 @@ router.get('/:id', (req, res) => {
       attributes: ['category_id']
     }
   })
-  .then(categoryData => res.json(categoryData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
@@ -45,11 +45,11 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
-  .then(categoryData => res.json(categoryData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
@@ -57,22 +57,22 @@ router.put('/:id', (req, res) => {
   Category.update({
     category_name: req.body.category_name
   },
-  {
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(categoryData => {
-    if (!categoryData) {
-      res.status(404).json({ message: 'There is no category with that ID.' });
-      return;
-    }
-    res.json(categoryData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'There is no category with that ID.' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
@@ -82,17 +82,17 @@ router.delete('/:id', (req, res) => {
       id: req.params.id
     }
   })
-  .then(categoryData => {
-    if (!categoryData) {
-      res.status(404).json({ message: 'There is no category with that ID.' });
-      return;
-    }
-    res.json(categoryData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'There is no category with that ID.' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
